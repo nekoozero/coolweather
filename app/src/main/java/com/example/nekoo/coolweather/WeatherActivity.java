a836446997f58b88b2cad10cea25215b4c3deec7
nekoozero <781869463@qq.com>
2018-04-11 23:54:39 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/nekoo/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/nekoo/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.nekoo.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.nekoo.coolweather.gson.Forecast;
 import com.example.nekoo.coolweather.gson.Weather;
+import com.example.nekoo.coolweather.service.AutoUpdateService;
 import com.example.nekoo.coolweather.util.HttpUtil;
 import com.example.nekoo.coolweather.util.Utility;
 
@@ -201,6 +203,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        //激活AutoUpdateService服务
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
 
     }
 
